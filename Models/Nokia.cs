e8292bb5ab78d14ead7849c17c9e047ebd4c498e
Jadson Sobrinho <jadsonsilva34677@gmail.com>
2024-06-02 15:46:24 -0300

    Implementação de classes por herança e metodos overrides

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+       public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+       {
+
+       }
+      public override void InstalarAplicativo(string nomeApp)
+        {
+           Console.WriteLine($"Intalando aplicatvo {nomeApp} no Nokia");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO.Services
+{
+
+    public class Teste
+    {
+        
+
+        public static void Main(string[] args)
+        {
+            Nokia NokiaC30 = new Nokia("554651651", "C30", "123456789", 64);
+            Iphone Iphone14 = new Iphone("1515165", "14", "987654321", 128);
+
+            NokiaC30.InstalarAplicativo("WhatsApp");
+            Iphone14.InstalarAplicativo("Android Services");
+        }
+        
+
+    
+    }
+
+}  
+
+
